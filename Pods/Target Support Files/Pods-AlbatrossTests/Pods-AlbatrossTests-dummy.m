6a29f134ddd87010624ca93fc5e3cac75ee66995
Daniel Hsu <dannyighsu@gmail.com>
2018-04-06 14:13:12 -0400

    Add reactive swift

---
+++ Pods/Target Support Files/Pods-AlbatrossTests/Pods-AlbatrossTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlbatrossTests : NSObject
+@end
+@implementation PodsDummy_Pods_AlbatrossTests
+@end
---
+++ Pods/Target Support Files/Pods-AlbatrossUITests/Pods-AlbatrossUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlbatrossUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AlbatrossUITests
+@end
